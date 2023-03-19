b63f51e1d560e6956ab5744f334fa80f35263afb
Fernando Bruno <fernandobrunoslima@gmail.com>
2023-03-19 10:48:22 -0300

    resolucao do exercicio 1 ao 5

--- exercicios.js
+++ exercicios.js
@@ -3,106 +3,113 @@
 // EXERCÍCIO 0A
 function soma(num1, num2) {
   // implemente sua lógica aqui
-  return num1 + num2
+  return num1 + num2;
 }
 
 // EXERCÍCIO 0B
 function imprimeMensagem() {
   // implemente sua lógica aqui
-  const mensagem = prompt('Digite uma mensagem!')
+  const mensagem = prompt("Digite uma mensagem!");
 
-  console.log(mensagem)
+  console.log(mensagem);
 }
 
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
-  // implemente sua lógica aqui
+  const altura = Number(prompt("Digite a altura do retângulo:"));
+  const largura = Number(prompt("Digite a largura do retângulo:"));
+  const area = altura * largura;
 
+  console.log(area);
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
-  // implemente sua lógica aqui
+  const anoAtual = Number(prompt("Insira o ano atual:"));
+  const anoNascimento = Number(prompt("Insira o ano de nascimento:"));
+  const idade = anoAtual - anoNascimento;
 
+  console.log(idade);
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  // implemente sua lógica aqui
+  const imc = peso / (altura * altura);
 
+  return imc;
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
+  const name = prompt("Qual é o seu nome?");
+  const age = Number(prompt("Informe a sua idade:"));
+  const email = prompt("Informe o seu email: ");
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+  console.log(
+    `Meu nome é ${name}, tenho ${age} anos, e o meu email é ${email}.`
+  );
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
+  const color1 = prompt("Qual sua primeira cor favorita?");
+  const color2 = prompt("Qual sua segunda cor favorita?");
+  const color3 = prompt("Qual sua terceira cor favorita?");
+  const colors = [color1, color2, color3];
 
+  console.log(colors);
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
-
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
-
 }
