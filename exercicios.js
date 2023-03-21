30cfb1540f68f67c9e210e8fc9f0e77bfff5c1bb
Fernando Bruno <fernandobrunoslima@gmail.com>
2023-03-21 07:08:03 -0300

    resolução de todos os itens

--- exercicios.js
+++ exercicios.js
@@ -97,24 +97,61 @@ function retornaUltimoElemento(array) {
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
+  array.unshift(array[array.length - 1]);
+  array.pop();
+  array.push(array[1]);
+  array.splice(1, 1);
+
+  return array;
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
+  return string1.toLowerCase() === string2.toLowerCase();
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+  const currentYear = Number(prompt("Informe o ano atual:"));
+  const birthYear = Number(prompt("Informe seu ano de nascimento:"));
+  const rgIssueYear = Number(prompt("Informe a data de emissão do RG"));
+  const userAge = currentYear - birthYear;
+  const rgYears = currentYear - rgIssueYear;
+  let renovationNeeded = false;
+
+  if (userAge <= 20 && rgYears >= 5) renovationNeeded = true;
+  if (userAge > 20 && userAge <= 50 && rgYears >= 10) renovationNeeded = true;
+  if (userAge > 50 && rgYears >= 15) renovationNeeded = true;
+
+  console.log(renovationNeeded);
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
+  let isLeapYear = false;
+  if (ano % 400 === 0) isLeapYear = true;
+  if (ano % 4 === 0 && ano % 100 !== 0) isLeapYear = true;
+
+  return isLeapYear;
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
+  let applianceValid = false;
+  const ageOver18 = prompt("Você tem mais de 18 anos?\nResponda sim ou não:");
+  const hasHighSchool = prompt(
+    "Você possui ensino médio completo?\nResponda sim ou não:"
+  );
+  const fullAvailable = prompt(
+    "Você possui disponibilidade exclusiva durante os horários do curso?\nResponda sim ou não:"
+  );
+
+  if (ageOver18 === "sim" && hasHighSchool === "sim" && fullAvailable === "sim")
+    applianceValid = true;
+
+  console.log(applianceValid);
 }
